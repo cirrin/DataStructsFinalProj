43d950a6cee60891d5757f25c1690ad44fe02f6d
Carson Hernandez <carson.hernandez@ufl.edu>
2024-11-30 21:56:42 -0500

    Minheap so far, CSV isnt set up yet

---
+++ MinHeap.h
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <stdexcept>
+#include <string>
+#include <sstream>
+#include <regex>
+#include <fstream>
+
+using namespace std;
+
+struct Song{
+    string date, country, track_id, artists, artist_genres, name;
+    int position, streams, duration;
+    bool explicit_song;
+
+    bool operator<(const Song& other) const{
+        return position < other.position;
+    }
+};
+
+class MinHeap{
+private:
+    vector<Song> heap;
+
+    int findParent(int i){
+        return (i - 1)/2;
+    }
+
+    int findLeftChild(int i){
+        return (2 * i) + 1;
+    }
+
+    int findRightChild(int i){
+        return (2 * i) + 2;
+    }
+
+    void heapifyUp(int i){
+        while(i > 0 && heap[i] < heap[findParent(i)]){
+            swap(heap[i], heap[findParent(i)]);
+            i = findParent(i);
+        }
+    }
+
+    string lowerCase(const string& search){
+        string lower = search;
+        transform(lower.begin(), lower.end(), lower.begin(), ::tolower);
+        return lower;
+    }
+
+public:
+    Song getMin(){
+        if (heap.empty()){
+            throw out_of_range("Heap is empty");
+        }
+        return heap[0];
+    }
+
+    bool isEmpty() const{
+        return heap.empty();
+    }
+
+    void insert(Song& song){
+        heap.push_back(song);
+        heapifyUp(heap.size() - 1);
+    }
+
+    Song searchByName(const string& name){
+        string search = lowerCase(name);
+        for(Song& song:heap){
+            if(lowerCase(song.name) == search){
+                return song;
+            }
+        }
+        throw invalid_argument("Song not found.");
+    }
+    //both searches are incomplete, still working on them due to CSV complications which havent been worked
+    vector<Song> searchByGenre(string& genre){
+        string searchGenre = lowerCase(genre);
+        vector<Song> matchingSongs;
+        for(Song& song:heap){
+            if(lowerCase(song.artist_genres).find(searchGenre) != string::npos){
+                matchingSongs.push_back(song);
+            }
+        }
+        sort(matchingSongs.begin(), matchingSongs.end(), [](const Song& a, const Song& b){
+            return a.streams > b.streams;
+        });
+        if(matchingSongs.size() > 5){
+            matchingSongs.resize(5);
+        }
+        return matchingSongs;
+    }
+
+
+};
--- main.cpp
+++ main.cpp
@@ -7,11 +7,12 @@
 #include <fstream>
 #include <map>
 #include <vector>
-
+#include <regex>
+#include "MinHeap.h"
 
 using namespace std;
 
-// splits string by delimiter ",". Returns a vector of each word.
+/*
 vector<string> splitString(string s) {
     vector<string> result;
     string token = "";
@@ -52,9 +53,62 @@ void readFileMaps(map<string, string> trackNames, map<string, string> artistName
 
     file.close();
 }
+}*/
+//i used a different method for splitting the CSV so that way it works for multiple commas in the brackets
+//please utilize this with the other stuff unless you have a better method
+vector<string> splitCSVLine(const string& line){
+    vector<string> fields;
+    string field;
+    bool inQuotes = false;
+    for(size_t i = 0; i < line.size(); i++){
+        char currentChar = line[i];
+        if(currentChar == '"'){
+            inQuotes = !inQuotes;
+        }
+        if(!inQuotes && currentChar == ',') {
+            fields.push_back(field);
+            field.clear();
+        }else{
+            field += currentChar;
+        }
+    }
+    fields.push_back(field);
+    return fields;
+}
 
-int main() {
-    cout << "Hello world" << endl;
-    map<string, string> trackNames, genres, artistNames;
-    readFileMaps(trackNames, artistNames, genres);
+int main(){
+    MinHeap minHeap;
+    ifstream file("../charts.csv");
+    if(!file.is_open()){
+        cerr << "Could not open the file!" << endl;
+        return 1;
+    }
+    string line;
+    string header;
+    getline(file, header);
+    while(getline(file, line)){
+        //trims leading or trailing whitespaces if necessary
+        line = regex_replace(line, regex("^\\s+|\\s+$"), "");
+        //splits the line into fields
+        vector<string> fields = splitCSVLine(line);
+        //process the fields
+        //CSV isnt set yet for the minheap, use the fields as their values if you can do it, feel free to change datatypes in song struct
+        if(fields.size() == 10){
+            cout << "Date: " << fields[0] << endl;
+            cout << "Country: " << fields[1] << endl;
+            cout << "Position: " << fields[2] << endl;
+            cout << "Streams: " << fields[3] << endl;
+            cout << "Track ID: " << fields[4] << endl;
+            cout << "Artists: " << fields[5] << endl;
+            cout << "Genres: " << fields[6] << endl;
+            cout << "Duration: " << fields[7] << endl;
+            cout << "Explicit: " << fields[8] << endl;
+            cout << "Name: " << fields[9] << endl;
+            cout << "Next Song: " << endl;
+        }else{
+            cerr << "Invalid line: " << line << endl;
+        }
+    }
+    file.close();
+    return 0;
 }
